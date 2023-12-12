5c9b1dba48c67277cd5bed475619cb1d78352be0
CamiloOrellanaaa <camiloorellana144@gmail.com>
2023-12-12 20:55:48 -0300

    Agregar css a addNameLaguna, FormName

--- src/components/AddNameLaguna.jsx
+++ src/components/AddNameLaguna.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./AddName.css";
 
 export const AddNameLaguna = ({ onCloseForm, onModify }) => {
   const [nuevoNombre, setNuevoNombre] = useState("");
@@ -19,20 +20,33 @@ export const AddNameLaguna = ({ onCloseForm, onModify }) => {
 
   return (
     <div>
-      <h2>Ingresa el nombre de la laguna</h2>
+      <h3 className="IngresaNombre my-5">Ingresa el nombre de la laguna</h3>
       <form onSubmit={agregarNombre}>
-        <label htmlFor="nombre">Nombre:</label>
-        <input
-          type="text"
-          id="nombre"
-          name="nombre"
-          value={nuevoNombre}
-          onChange={handleInputChange}
-        />
-        <br />
-        <button type="submit" onClick={() => onModify(nuevoNombre)}>
-          Enviar
-        </button>
+        <div className="row container">
+          <div className="col-1"></div>
+          <div className="col-2 nombre">
+            <label htmlFor="nombre">Nombre:</label>
+          </div>
+          <div className="col-8">
+            <input
+              className="input form-control"
+              type="text"
+              id="nombre"
+              name="nombre"
+              value={nuevoNombre}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div className="d-flex justify-content-center">
+          <button
+            className="send border-0 bg-secondary rounded-3 ps-2 pe-3 fs-4 text-white my-4"
+            type="submit"
+            onClick={() => onModify(nuevoNombre)}
+          >
+            Enviar
+          </button>
+        </div>
       </form>
     </div>
   );
--- src/components/FormName.jsx
+++ src/components/FormName.jsx
@@ -1,3 +1,5 @@
+import "./AddName.css";
+
 const dialog = {
   width: "100%",
   height: "100%",
@@ -6,8 +8,8 @@ const dialog = {
 };
 
 const content = {
-  height: "50%",
-  width: "30%",
+  height: "40%",
+  width: "40%",
   position: "fixed",
   top: "5%",
   left: "50%",
@@ -27,7 +29,7 @@ export const FormName = ({ children, onOpen, onClose }) => {
       ></button>
       <div className="bg-dark rounded-4" style={content}>
         <div className="text-end p-2">
-          <button className="border-0 bg-dark float-end pe-2" onClick={onClose}>
+          <button className="border-0 bg-dark float-end pe-2 send" onClick={onClose}>
             <img src="https://img.icons8.com/?size=30&id=13107&format=png" />
           </button>
         </div>
@@ -36,3 +38,4 @@ export const FormName = ({ children, onOpen, onClose }) => {
     </dialog>
   );
 };
+
